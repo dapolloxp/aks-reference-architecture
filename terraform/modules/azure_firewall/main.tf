5da78c6b613d549421990ff22708c763f289cf4b
Dylan Reed <71025787+djr1991@users.noreply.github.com>
2021-09-29 16:33:43 -0400

    fix FW service tag rules by removing region

--- terraform/modules/azure_firewall/main.tf
+++ terraform/modules/azure_firewall/main.tf
@@ -88,16 +88,16 @@ resource "azurerm_monitor_diagnostic_setting" "azfw_diag" {
 resource "azurerm_firewall_policy_rule_collection_group" "hub_to_spoke_rule_collection" {
   name               = "hub-to-spoke-afwpolicy-rcg"
   firewall_policy_id = azurerm_firewall_policy.base_policy.id
-  priority           = 100
+  priority           = 110
 
   network_rule_collection {
-    name     = "hub_to_spoke_network_rule_collection"
+    name     = "hub-to-spoke-network-rule-collection"
     priority = 100
     action   = "Allow"
 
     rule {
-      name                  = "hub_to_spoke_global_network_rule"
-      protocols             = ["TCP"]
+      name                  = "hub-to-spoke-global-network-rule"
+      protocols             = ["Any"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
       destination_ip_groups = [var.region1_hub_ip_g_id]
       destination_ports     = ["*"]
@@ -110,12 +110,12 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
   firewall_policy_id = azurerm_firewall_policy.base_policy.id
   priority           = 200
   application_rule_collection {
-    name     = "aks_app_rule_collection"
+    name     = "aks-app-rule-collection"
     priority = 200
     action   = "Allow"
 
     rule {
-      name = "aks_service_tag"
+      name = "aks-service-tag"
       protocols {
         type = "Https"
         port = 443
@@ -126,7 +126,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     
 
     rule {
-      name = "ubuntu_libraries"
+      name = "ubuntu-libraries"
       protocols {
         type = "Https"
         port = 443
@@ -136,7 +136,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     }
 
     rule {
-      name = "microsoft_crls"
+      name = "microsoft-crls"
       protocols {
         type = "Http"
         port = 80
@@ -149,7 +149,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     }
 
     rule {
-      name = "github_rules"
+      name = "github-rules"
       protocols {
         type = "Https"
         port = 443
@@ -159,7 +159,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     }
 
     rule {
-      name = "microsoft_metrics_rules"
+      name = "microsoft-metrics-rules"
       protocols {
         type = "Https"
         port = 443
@@ -169,7 +169,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     }
 
     rule {
-      name = "aks_acs_rules"
+      name = "aks-acs-rules"
       protocols {
         type = "Https"
         port = 443
@@ -182,7 +182,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
     }
 
     rule {
-      name = "microsoft_login_rules"
+      name = "microsoft-login-rules"
       protocols {
         type = "Https"
         port = 443
@@ -193,20 +193,20 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks_rule_collection" {
   }
 
   network_rule_collection {
-    name     = "aks_network_rule_collection"
+    name     = "aks-network-rule-collection"
     priority = 100
     action   = "Allow"
 
     rule {
-      name                  = "aks_global_network_rule"
+      name                  = "aks-global-network-rule"
       protocols             = ["TCP"]
       source_ip_groups = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns = ["AzureCloud"]
+      destination_addresses = ["AzureCloud"]
       destination_ports     = ["443", "9000"]
     }
 
     rule {
-      name                  = "aks_ntp_network_rule"
+      name                  = "aks-ntp-network-rule"
       protocols             = ["UDP"]
       source_ip_groups = [var.region1_mlw_spk_ip_g_id]      
       destination_addresses = ["*"]
@@ -220,7 +220,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "mlw_rule_collection" {
   firewall_policy_id = azurerm_firewall_policy.base_policy.id
   priority           = 300
   application_rule_collection {
-    name     = "mlw_app_rule_collection"
+    name     = "mlw-app-rule-collection"
     priority = 200
     action   = "Allow"
 
@@ -380,71 +380,71 @@ resource "azurerm_firewall_policy_rule_collection_group" "mlw_rule_collection" {
   }
 
   network_rule_collection {
-    name     = "mlw_network_rule_collection"
+    name     = "mlw-network-rule-collection"
     priority = 100
     action   = "Allow"
 
     rule {
-      name                  = "Azure_Active_Directory"
+      name                  = "Azure-Active-Directory"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureActiveDirectory"]
+      destination_addresses = ["AzureActiveDirectory"]
       destination_ports     = ["*"]
     }
 
     rule {
-      name                  = "Azure_Machine_Learning"
+      name                  = "Azure-Machine-Learning"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureMachineLearning"]
+      destination_addresses     = ["AzureMachineLearning"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Azure_Resource_Manager"
+      name                  = "Azure-Resource-Manager"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureResourceManager"]
+      destination_addresses = ["AzureResourceManager"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Azure_Storage"
+      name                  = "Azure-Storage"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["Storage.${var.location}"]
+      destination_addresses = ["Storage"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Azure_Front_Door_Frontend"
+      name                  = "Azure-Front-Door-Frontend"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureFrontDoor.Frontend"]
+      destination_addresses = ["AzureFrontDoor.Frontend"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Azure_Container_Registry"
+      name                  = "Azure-Container-Registry"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureContainerRegistry.${var.location}"]
+      destination_addresses = ["AzureContainerRegistry"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Azure_Key_Vault"
+      name                  = "Azure-Key-Vault"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["AzureKeyVault.${var.location}"]
+      destination_addresses = ["AzureKeyVault"]
       destination_ports     = ["443"]
     }
 
     rule {
-      name                  = "Microsoft_Container_Registry"
+      name                  = "Microsoft-Container-Registry"
       protocols             = ["TCP"]
       source_ip_groups      = [var.region1_mlw_spk_ip_g_id]      
-      destination_fqdns     = ["MicrosoftContainerRegistry.${var.location}"]
+      destination_addresses = ["MicrosoftContainerRegistry"]
       destination_ports     = ["443"]
     }
   }    
