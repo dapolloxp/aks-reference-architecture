ee33d3133105a30f7c3aec5d4c58b9f68a783391
David Apolinar <dapolloxp@users.noreply.github.com>
2021-11-02 14:42:55 -0400

    Update tools_install.sh

--- terraform/modules/jump_host/tools_install.sh
+++ terraform/modules/jump_host/tools_install.sh
@@ -24,6 +24,15 @@ echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt
 sudo apt-get update -y
 sudo apt-get install helm -y
 
+# Install .net core
+
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update;  sudo apt-get install -y apt-transport-https &&   sudo apt-get update &&   sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get update; sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y aspnetcore-runtime-3.1
+
+
 
 # Generate SSH key
 
